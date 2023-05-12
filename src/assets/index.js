8f887943f580a3ab99157f6146d62895567e2c1f
Manash2712 <manashchauhan2712@gmail.com>
2023-05-12 23:37:12 +0530

    completed navbar

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+
 import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./mobile.png";
@@ -30,8 +30,10 @@ import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
+import logoSvg from "./logoSvg.svg"
+
 export {
-  logo,
+  logoSvg,
   backend,
   creator,
   mobile,
