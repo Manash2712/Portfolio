581a25ab62567d72fbd1452044d27a2f2057b0b5
Manash2712 <manashchauhan2712@gmail.com>
2023-05-14 02:03:20 +0530

    added gif maded with motion and used isMobile state variable for checking the screen size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,13 +18,31 @@ const Hero = () => {
                         Hi, I'm <span className="text-[#915eff]">Manash</span>
                     </h1>
                     <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-                        Lorem ipsum dolor sit amet{" "}
+                        Lorem ipsum dolor sit amet
                         <br className="sm:block hidden" />
                         consectetur.
                     </p>
                 </div>
             </div>
             <ComputersCanvas />
+
+            <div className="absolute xs:bottom-2 bottom-2 w-full flex justify-center items-center">
+                <a href="#about">
+                    <div className="w-[30px] h-[54px] sm:w-[35px] sm:h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+                        <motion.dev
+                            animate={{
+                                y: [0, 24, 0],
+                            }}
+                            transition={{
+                                duration: 1.5,
+                                repeat: Infinity,
+                                repeatType: "loop",
+                            }}
+                            className="w-3 h-3 rounded-full bg-secondary mb-1"
+                        />
+                    </div>
+                </a>
+            </div>
         </section>
     );
 };
